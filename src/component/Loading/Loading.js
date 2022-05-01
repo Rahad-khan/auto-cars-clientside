655188d912dcdf904e5051cd84ed882bb111503b
Rahad Khan <clash146098@gmail.com>
2022-05-01 23:43:10 +0600

    google and fb login addeded

--- src/component/Loading/Loading.js
+++ src/component/Loading/Loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = () => {
     return (
-      <div className="flex justify-center items-center space-x-2">
+      <div className="flex justify-center items-center space-x-2 my-3">
         <div
           className="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-blue-600"
           role="status"
--- src/component/SocialLogin/SocialLogin.js
+++ src/component/SocialLogin/SocialLogin.js
@@ -3,37 +3,58 @@ import { BsGoogle } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 import { useSignInWithGoogle, useSignInWithFacebook } from "react-firebase-hooks/auth";
 import auth from '../../firebase.init';
+import Loading from '../Loading/Loading';
 const SocialLogin = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    const [signInWithFacebook, fbUser, fbLoading, fbError] =
-      useSignInWithFacebook(auth);
-    return (
-      <div>
-        <div className="flex items-center my-4">
-          <div style={{ height: "2px" }} className="w-full bg-blue-900"></div>
-          <div className="px-3">OR</div>
-          <div style={{ height: "2px" }} className="w-full bg-blue-900"></div>
-        </div>
-        <button
-          className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3 flex justify-center items-center bg-[#4285F4] hover:bg-[#4285F4]/90"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="light"
-        >
-          {/* <!-- Twitter --> */}
-          <BsGoogle className="w-3.5 h-3.5 mr-2"></BsGoogle>
-          Continue with Google
-        </button>
-        <button
-          className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center bg-[#3b5998] hover:bg-[#3b5998]/90"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="light"
-        >
-          {/* <!-- Facebook --> */}
-          <FaFacebookF className="w-3.5 h-3.5 mr-2"></FaFacebookF>
-          Continue with Facebook
-        </button>
-      </div>
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithFacebook, fbUser, fbLoading, fbError] =
+    useSignInWithFacebook(auth);
+
+  // error message
+  let errorMessage;
+  let processing;
+  //   If give error
+  if (error || fbError) {
+    errorMessage = (
+      <p className="text-red-700 mb-4">{error?.message || fbError?.message}</p>
     );
+  }
+  //   If loading
+  if (loading || fbLoading) {
+    errorMessage = "";
+    processing = <Loading></Loading>;
+  }
+  return (
+    <div>
+      <div className="flex items-center my-4">
+        <div style={{ height: "2px" }} className="w-full bg-blue-900"></div>
+        <div className="px-3">OR</div>
+        <div style={{ height: "2px" }} className="w-full bg-blue-900"></div>
+      </div>
+      {processing}
+      {errorMessage}
+
+      <button
+        onClick={() => signInWithGoogle()}
+        className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3 flex justify-center items-center bg-[#4285F4] hover:bg-[#4285F4]/90"
+        data-mdb-ripple="true"
+        data-mdb-ripple-color="light"
+      >
+        {/* <!-- Google --> */}
+        <BsGoogle className="w-3.5 h-3.5 mr-2"></BsGoogle>
+        Continue with Google
+      </button>
+      <button
+        onClick={() => signInWithFacebook()}
+        className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center bg-[#3b5998] hover:bg-[#3b5998]/90"
+        data-mdb-ripple="true"
+        data-mdb-ripple-color="light"
+      >
+        {/* <!-- Facebook --> */}
+        <FaFacebookF className="w-3.5 h-3.5 mr-2"></FaFacebookF>
+        Continue with Facebook
+      </button>
+    </div>
+  );
 };
 
 export default SocialLogin;
