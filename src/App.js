8293706e4c7066e3c851c9cfea304706791cd47d
Rahad Khan <clash146098@gmail.com>
2022-05-01 13:39:49 +0600

    error page addeded

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Error from './component/Error/Error';
 import Footer from './component/Footer/Footer';
 import Header from './component/Header/Header';
 import Home from './component/Home/Home';
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='*' element={<Error/>}></Route>
       </Routes>
       <Footer/>
     </div>
---
+++ src/component/Error/Error.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Error.css'
+
+const Error = () => {
+    
+
+    return (
+      <div id="error">
+        <div id="error-container">
+          <div class="fof">
+            <h1>Error 404</h1>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Error;
